fa9297a5e1ca7d1045690787153b8d285e59523e
Jader Dias <jader.dias@booking.com>
2015-08-29 09:58:53 +0200

    renamed files go working

--- 1parallel.go
+++ 1parallel.go
@@ -5,12 +5,15 @@ import (
     "time"
 )
 
+const limit = 1e3
+
 func main() {
     semaphore := make(chan int)
 	finished := make(chan int)
     go Crawl(1, semaphore, finished)
 
-	for id := range make([]int, 1 + 2e3)  {
+	for id := range make([]int, 1 + (2 * limit))  {
+        <-finished
         log.Println(id, "finished")
 	}
 
@@ -22,7 +25,7 @@ func Crawl(id int, semaphore <-chan int, finished chan<- int) {
 	log.Println(id, "start")
     finished <- id
     log.Println("\t", id, "answered")
-	if id < 1e3 {
+	if id <= limit {
 		go Crawl(2*id, semaphore, finished)
         time.Sleep(1)
 		Crawl((2*id)+1, semaphore, finished)
